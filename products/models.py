c78c1f0215b97625b547eab7c3ce9a570310aaff
Vileblood <david.thomas1986@gmail.com>
2020-08-14 18:26:07 +0000

    cut down on images to display

--- products/models.py
+++ products/models.py
@@ -2,12 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Category(models.Model):
-    genre = models.CharField(max_length=254)
-
-    def __str__(self):
-        return self.name
-
 
 class Product(models.Model):
     name = models.CharField(max_length=254)
@@ -17,6 +11,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    genre = models.CharField(max_length=254)
+
     
     
     def __str__(self):
