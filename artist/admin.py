0604bf892e6cd55d40550ed9cf561625239c00ef
Vileblood <david.thomas1986@gmail.com>
2020-08-17 21:37:04 +0000

    customised admin

--- artist/admin.py
+++ artist/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from .models import Artist
 # Register your models here.
 
-admin.site.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'nationality',
+        'birth',
+        'death',
+        'genre',
+        'bio',
+        'image',
+
+    )
+
+
+
+admin.site.register(Artist, ArtistAdmin)
--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Painting
 # Register your models here.
 
-admin.site.register(Painting)
+
+class PaintingAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'year',
+        'price',
+        'image',
+    )
+
+
+
+admin.site.register(Painting, PaintingAdmin)
