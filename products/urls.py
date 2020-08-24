c89252aecd75237e1e0e674690582dfd6ca39ce1
Vileblood <david.thomas1986@gmail.com>
2020-08-24 21:34:24 +0000

    added products and paintings details views and url

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
-
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.paintings_detail, name='paintings_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Painting
 
 # Create your views here.
@@ -8,6 +8,22 @@ def all_products(request):
     """a view to show all products including search"""
 
     products = Painting.objects.all()
+
+    for p in products:
+        print(p)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/paintings.html', context)
+
+
+
+    def paintings_detail(request):
+        """a view to show individual paintings"""
+
+    product = get_object_or_404(Painting, pk=product_id)
 
     for p in products:
         print(p)
