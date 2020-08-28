838a1f4caacf19b994134a8105b0c621e7874de0
Vileblood <david.thomas1986@gmail.com>
2020-08-28 22:27:39 +0000

    added +/- to quantity

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.painting_detail, name='painting_detail'),
-    path('products/<genre_slug>/', views.genre, name='genre'),
-    path('products/products/impressionism', views.impressionism, name='impressionism'),
+    
 ]
