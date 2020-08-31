118539b12bfc0a704ad4ecf2a7f44c5f2a6e2585
Vileblood <david.thomas1986@gmail.com>
2020-08-31 09:12:35 +0000

    updated checkout form

--- checkout/models.py
+++ checkout/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
-from django_countries.fields import CountryField 
+from django_countries.fields import CountryField
 
 from products.models import Painting
 
